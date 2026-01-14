b6c3cdbeaa6e75d3de1221eb8427ac039920b71f
hangryshiba-MacAir <prom.p2001@gmail.com>
2026-01-15 00:54:34 +0700

    checkpoint s.2-39

--- starting-project/starting-project/src/components/PostList.jsx
+++ starting-project/starting-project/src/components/PostList.jsx
@@ -6,12 +6,15 @@ import Modal from './Modal.jsx';
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch('http://localhost:8080/posts');
       const resData = await response.json();
 
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -34,7 +37,7 @@ function PostList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post, index) => {
             return <Post key={index} author={post.author} body={post.body} />;
@@ -42,10 +45,16 @@ function PostList({ isPosting, onStopPosting }) {
         </ul>
       )}
 
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no post yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <p>Loading Post...</p>
         </div>
       )}
     </>
