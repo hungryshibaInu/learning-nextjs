df7a85a867fb1f3dcfb0c2a4f14e1f2a99e34b81
hangryshiba-MacAir <prom.p2001@gmail.com>
2026-01-26 22:39:21 +0700

    checkpoint s.2-44

--- starting-project/starting-project/src/components/PostList.jsx
+++ starting-project/starting-project/src/components/PostList.jsx
@@ -1,24 +1,9 @@
-import { useState, useEffect } from 'react';
 import Post from './Post.jsx';
 import classes from './PostList.module.css';
-import NewPost from './NewPost.jsx';
-import Modal from './Modal.jsx';
+import { useLoaderData } from 'react-router-dom';
 
-function PostList({ isPosting, onStopPosting }) {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch('http://localhost:8080/posts');
-      const resData = await response.json();
-
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
+function PostList() {
+  const posts = useLoaderData();
 
   function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', {
@@ -31,13 +16,7 @@ function PostList({ isPosting, onStopPosting }) {
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
-
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post, index) => {
             return <Post key={index} author={post.author} body={post.body} />;
@@ -45,16 +24,10 @@ function PostList({ isPosting, onStopPosting }) {
         </ul>
       )}
 
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no post yet.</h2>
           <p>Start adding some!</p>
-        </div>
-      )}
-
-      {isFetching && (
-        <div style={{ textAlign: 'center', color: 'white' }}>
-          <p>Loading Post...</p>
         </div>
       )}
     </>
